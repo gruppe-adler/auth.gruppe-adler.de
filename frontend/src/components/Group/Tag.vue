5026247662dc3c9811da975c51e5531a59821deb
simon.schmidt <simon.schmidt@idealo.de>
2019-07-24 11:26:14 +0200

    pill hover

--- frontend/src/components/Group/Tag.vue
+++ frontend/src/components/Group/Tag.vue
@@ -82,6 +82,7 @@ export default class GroupTag extends Vue {
     &:hover &__star,
     &:hover &__delete {
         display: initial !important;
+        transition: all .2s cubic-bezier(0.455, 0.03, 0.515, 0.955);
     }
 
     > span {
